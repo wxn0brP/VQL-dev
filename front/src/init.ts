0e4204d635130653707381a66c8d91eb851fe6ce
wxn0brP <toja.filiper@gmail.com>
2025-06-20 10:01:46 +0200

    feat: update url query params

--- front/src/init.ts
+++ front/src/init.ts
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-let param = urlParams.get("server") || urlParams.get("url") || urlParams.get("s");
+let param = urlParams.get("server") || urlParams.get("url") || urlParams.get("s") || urlParams.get("p") || urlParams.get("port");
 if (param && !isNaN(parseInt(param))) param = "http://localhost:" + param;
 
 export const defaultFetchUrl = param || "http://localhost:3000";
