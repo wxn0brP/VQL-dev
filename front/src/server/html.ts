63d68999fea2ceb5f7639baadc46edc30a8ddd12
wxn0brP <toja.filiper@gmail.com>
2025-11-18 18:40:38 +0100

    feat: local database data

--- front/src/server/html.ts
+++ front/src/server/html.ts
@@ -1,18 +1,18 @@
 import { doConfig } from "./config";
 import { db } from "./vql";
 
-export const textarea = document.querySelector("textarea");
-export const ul = document.querySelector("ul");
-export const memoryDbTextarea = document.querySelector<HTMLTextAreaElement>("#memory-db");
+export const textarea = qs<HTMLTextAreaElement>("textarea");
+export const ul = qs<HTMLUListElement>("ul");
+export const memoryDbTextarea = qs<HTMLTextAreaElement>("#memory-db");
 
-document.querySelector("#run").addEventListener("click", () => doConfig());
+qs("#run").addEventListener("click", () => doConfig());
 textarea.addEventListener("keyup", (e) => {
     if (e.shiftKey && e.key === "Enter") {
         doConfig();
     }
 });
 
-document.querySelector("#save").addEventListener("click", () => {
+qs("#save").addEventListener("click", () => {
     const data = textarea.value;
     localStorage.setItem("vql-server-config", data);
 });
@@ -22,19 +22,15 @@ function dumpMemoryDb() {
     memoryDbTextarea.value = JSON.stringify(Object.fromEntries(memoryAction.memory.entries()), null, 2);
 }
 
-document.querySelector("#memory-db-dump").addEventListener("click", () => dumpMemoryDb());
-
-document.querySelector("#memory-db-load").addEventListener("click", () => {
+function loadMemoryDb() {
     try {
         const memoryAction = db.dbAction as any;
         const data = JSON.parse(memoryDbTextarea.value);
         memoryAction.memory = new Map(Object.entries(data));
-        alert("Memory DB loaded!");
     } catch (e) {
         console.error(e);
-        alert("Error loading data into memory DB. Check console.");
     }
-});
+}
 
 setTimeout(() => {
     const data = localStorage.getItem("vql-server-config");
@@ -44,3 +40,13 @@ setTimeout(() => {
     }
     dumpMemoryDb();
 }, 100);
+
+memoryDbTextarea.addEventListener("input", () => loadMemoryDb());
+memoryDbTextarea.addEventListener("change", () => loadMemoryDb());
+db.emiter.on("*", () => dumpMemoryDb());
+
+qs("#memory-db-load-preset").addEventListener("click", async () => {
+    const preset = await fetch("example.json").then((res) => res.json());
+    memoryDbTextarea.value = JSON.stringify(preset, null, 2);
+    loadMemoryDb();
+});
