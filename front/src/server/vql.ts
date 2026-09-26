72058b5d9bd6f41dc6cafb9401550fdc985938e6
wxn0brP <viviennevaltheris@gmail.com>
2026-09-26 19:07:38 +0200

    fix: web server local db

--- front/src/server/vql.ts
+++ front/src/server/vql.ts
@@ -2,5 +2,5 @@ import { createMemoryValthera } from "@wxn0brp/db-core";
 import VQLProcessor from "@wxn0brp/vql";
 export const db = createMemoryValthera();
 export const VQL = new VQLProcessor({
-	db,
+	local: db,
 } as any);
